89fea833c4e9f3ff7be6b04f7665b078bb151a45
David Metzler <david-metzler@web.de>
2024-11-18 17:51:55 +0100

    Properly configure trouble.nvim

---
+++ lua/configs/trouble.lua
@@ -0,0 +1,4 @@
+local opts = {
+  auto_close = true, -- auto close when there are no items
+}
+return opts
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -37,6 +37,31 @@ local mappings = {
       },
     },
 
+    ["<leader>ix"] = {
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    ["<leader>iX"] = {
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    ["<leader>is"] = {
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    ["<leader>il"] = {
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / References / ... (Trouble)",
+    },
+    ["<leader>iL"] = {
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    ["<leader>iQ"] = {
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+
     -- ["<leader>fm"] = {
     --   function()
     --     vim.lsp.buf.format { async = true }
