7ee55b68ccf5e6d4234c308ed90714a9ea2aafa7
David Metzler <david-metzler@web.de>
2024-12-20 10:50:56 +0100

    Better dapui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -9,7 +9,56 @@ return {
     config = function()
       local dap = require "dap"
       local dapui = require "dapui"
-      dapui.setup()
+      dapui.setup {
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "»" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>", "<TAB>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.5 },
+              { id = "stacks", size = 0.3 },
+              { id = "breakpoints", size = 0.10 },
+              { id = "watches", size = 0.10 },
+            },
+            position = "left",
+            size = 30,
+          },
+          {
+            elements = {
+              { id = "console", size = 0.8 },
+              { id = "repl", size = 0.2 },
+            },
+            position = "bottom",
+            size = 20,
+          },
+        },
+        controls = {
+          -- Requires Neovim nightly (or 0.8 when released)
+          enabled = true, -- because the icons don't work
+          -- Display controls in this element
+          element = "console",
+          icons = {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+          },
+        },
+        windows = { indent = 1 },
+      }
+
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
         vim.cmd "NvimTreeClose"
